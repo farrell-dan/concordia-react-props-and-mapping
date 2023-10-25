7b1019e3455f307504c85bbcf695bfcd6ca8f567
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-25 14:22:42 -0400

    Exercise 6

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,18 @@ const App = ({users}) => {
             <h1>The users</h1>
             <ul>
                 {users.map((user) => {
-                    console.log(user);
+                    // console.log(user);
                     return (
                         <li key={user.id}>
                             <h2>{user.name}</h2>
                             <p>age: {user.age}</p>
+                            <ul>
+                                <h3>Skills and Traits</h3>
+                                {user["skills and traits"].map((skill, index) => {
+                                    return <li key={index+1}>{skill}</li>;})}
+                            </ul>
                             <p>A character from {user.fiction} - <a href={user.wiki} target="_blank">more info</a></p>
+
                         </li>
 
 
