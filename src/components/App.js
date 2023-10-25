34b964d268b6d28cb607ee40861fda21aeb22e3a
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-25 14:08:30 -0400

    Exercise 5

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 const App = ({users}) => {
-    console.log(users);
     return (
-        <main></main>
+        <main>
+            <h1>The users</h1>
+            <ul>
+                {users.map((user) => {
+                    console.log(user);
+                    return (
+                        <li key={user.id}>
+                            <h2>{user.name}</h2>
+                            <p>age: {user.age}</p>
+                            <p>A character from {user.fiction} - <a href={user.wiki} target="_blank">more info</a></p>
+                        </li>
+
+
+                    )
+                })}
+            </ul>
+        </main>
     )
 }
 
